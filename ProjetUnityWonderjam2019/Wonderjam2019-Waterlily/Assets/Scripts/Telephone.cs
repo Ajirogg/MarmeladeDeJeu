7d58696faab0bb88fc28cfb1a98716dae6b1161d
Ajirogg <raimundo.guillaume@gmail.com>
2019-03-01 21:27:07 -0500

    Telephone
    Creation du telephone et des fonctions de debut d'appel et de debut de conversation (a modifier)

---
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Telephone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telephone : MonoBehaviour
+{
+    public bool isRinging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRinging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartCall()
+    {
+        isRinging = true;
+    }
+
+    public void ConversationBegin()
+    {
+        isRinging = false;
+        // Doit appeler la fonction de discussion entre police et preneur d'otages
+    }
+}

79e83f83e8a23a6c12878f068a4e78d664230d82
Ajirogg <raimundo.guillaume@gmail.com>
2019-03-02 10:17:58 -0500

    Revert "Revert "Telephone lier au GM""
    This reverts commit b61f6bb8d3d6e2a144958b19e01ac42e811bb196.

--- ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Telephone.cs
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Telephone.cs
@@ -5,36 +5,35 @@ using UnityEngine;
 public class Telephone : MonoBehaviour, Utilisable
 {
     public bool isRinging;
-    public int timeToAnswer;
-    public float timeStartCall;
 
     // Start is called before the first frame update
     void Start()
     {
         isRinging = false;
-        timeToAnswer = Random.Range(10, 15 + 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> parent of c5770e6... Telephone et boucle d'appel
     }
 
     public void StartCall()
     {
         isRinging = true;
-        timeStartCall = Time.time;
-        print("Gaston y a le telefon qui son");
     }
 
     public void ConversationBegin()
     {
         isRinging = false;
-        print("ALLO");
         // Doit appeler la fonction de discussion entre police et preneur d'otages
     }
 
+<<<<<<< HEAD
     public void endCall()
     {
         isRinging = false;
@@ -43,6 +42,9 @@ public class Telephone : MonoBehaviour, Utilisable
     }
 
     public void Use()
+=======
+    public override void Use()
+>>>>>>> parent of c5770e6... Telephone et boucle d'appel
     {
         throw new System.NotImplementedException();
     }

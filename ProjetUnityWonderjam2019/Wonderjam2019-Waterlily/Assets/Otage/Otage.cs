62b3d50874e577b39f685f953e3c45a6bba48573
Xikcrem <kiki_br@hotmail.fr>
2019-03-02 03:43:18 -0500

    Groupe d'otages
    implémentation du prefab et script groupe d'otage qui vont gérer les otages
    et leur positions.

--- ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Otage/Otage.cs
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Otage/Otage.cs
@@ -9,7 +9,9 @@ public class Otage : Utilisable
     private float lastStressRaise ;
     public bool isYelling = false;
     public int quitChance = 20;
-    public int maxStressRaise = 5;
+    public int maxStressRaise = 10;
+    public int maxPanic = 200;
+    public bool talking = false;
 
     private void Start()
     {
@@ -19,8 +21,8 @@ public class Otage : Utilisable
     // Update is called once per frame
     void Update()
     {
-        if (lastStressRaise + stressPeriod <= Time.time) { 
-            if (panic < 100)
+        if (lastStressRaise + stressPeriod <= Time.time && !talking) { 
+            if (panic < maxPanic)
                 PanicRaise();
             else 
             {
@@ -35,26 +37,31 @@ public class Otage : Utilisable
         print("Augementation + " + raise);
         panic += raise;
         lastStressRaise = Time.time;
-        
-        if (! isYelling & panic >= 50)
-            isYelling = true ;
 
-        if (panic > 100)
-            panic = 100;
+        if (!isYelling & panic >= maxPanic/2)
+        {
+            isYelling = true;
+            GetComponentInParent<GroupeOtage>().addYelling();
+        }
+
+        if (panic > maxPanic)
+            panic = maxPanic;
         
     }
     public void PanicDecrease(int decrease)
     {
         panic -= decrease ;
-        if (isYelling & panic < 50)
+        if (isYelling & panic < maxPanic/2)
             isYelling = false;
     }
     public void RandQuit()
     {
         int rand = Random.Range(1, ((100 / quitChance) + 1));
         print("Part ? = " + rand);
-        if (rand == 100 / quitChance)
+        if (rand == 100 / quitChance) { 
             Destroy(this.gameObject);
+            GetComponentInParent<GroupeOtage>().nbOtage -=1 ;
+        }
         lastStressRaise = Time.time;
     }
     public override void Use()

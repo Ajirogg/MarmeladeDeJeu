42950aa59a232e41a5467d66166d1bd5d5ae29ce
Ajirogg <raimundo.guillaume@gmail.com>
2019-03-02 10:17:24 -0500

    Telephone lier au GM

--- ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Police.cs
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Police.cs
@@ -6,10 +6,8 @@ public class Police : MonoBehaviour
 {
 
     private int etatPolice;         // Enum de l'etat de la police de 1 à 5
-   // private int barreEtatPolice;    // Barre progressante permettant le changement d'état de 0 à 50
+    private int barreEtatPolice;    // Barre progressante permettant le changement d'état de 0 à 50
     private int agressivitePolice;  // Barre de progression de l'agressivité de la police 100 = défaite
-    public int frequenceAppel = 60; // Fréquence en seconde d'appel de la police
-    public int frequenceAppelMinimum = 60; // Fréquence en seconde d'appel de la police
 
 
 
@@ -17,32 +15,39 @@ public class Police : MonoBehaviour
     void Start()
     {
         etatPolice = 1;
-       // barreEtatPolice = 0;
+        barreEtatPolice = 0;
         agressivitePolice = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-    }
+        if (Input.GetKeyDown("q"))
+        {
+            AugmenterAgressivite(10);
+            AugmenterBarreEtat(5);
+            Debug.Log(agressivitePolice);
+            Debug.Log(barreEtatPolice);
+            Debug.Log(etatPolice);
 
-    public void PrintPoliceTest()
-    {
-        Debug.Log(agressivitePolice);
-        Debug.Log(etatPolice);
-        Debug.Log(frequenceAppel);
 
+        }
+        else if (Input.GetKeyDown("e"))
+        {
+            DiminuerAgressivite(10);
+            DiminuerBarreEtat(5);
+            Debug.Log(agressivitePolice);
+            Debug.Log(barreEtatPolice);
+            Debug.Log(etatPolice);
+
+        }
     }
 
     public void AugmenterAgressivite(int value)
     {
         agressivitePolice += value;
-        if(agressivitePolice >= etatPolice * 20)
-        {
-            ++etatPolice;
-            frequenceAppel = frequenceAppelMinimum - (frequenceAppelMinimum * etatPolice * 10 / 100);
-        }
+        if (agressivitePolice > 100)
+            agressivitePolice = 100;
     }
 
     public void DiminuerAgressivite(int value)
@@ -52,12 +57,7 @@ public class Police : MonoBehaviour
             agressivitePolice = 0;
     }
 
-    public int GetFrequenceAppel()
-    {
-        return frequenceAppel;
-    }
-
-  /*  public void AugmenterBarreEtat(int value)
+    public void AugmenterBarreEtat(int value)
     {
         barreEtatPolice += value;
         if (barreEtatPolice == etatPolice * 10 && etatPolice <5)
@@ -69,9 +69,7 @@ public class Police : MonoBehaviour
         barreEtatPolice -= value;
         if (barreEtatPolice < 0)
             barreEtatPolice = 0;
-    }*/
-
-
+    }
 
 
 

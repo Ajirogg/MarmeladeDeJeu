c5770e6610dfd74ae00d01fb05e5683c5e0d4bd3
Ajirogg <raimundo.guillaume@gmail.com>
2019-03-02 10:10:25 -0500

    Telephone et boucle d'appel
    Mise en place du téléphone et d"une boucle primaire d'appel de la police (juste appel debut conv fin conv avec possibilité de ne pas decrocher)

--- ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Telephone.cs
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Telephone.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class Telephone : Utilisable
 {
     public bool isRinging;
+    public int timeToAnswer;
+    public float timeStartCall;
 
     // Start is called before the first frame update
     void Start()
     {
         isRinging = false;
+        timeToAnswer = Random.Range(10, 15 + 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
 
     public void StartCall()
     {
         isRinging = true;
+        timeStartCall = Time.time;
+        print("Gaston y a le telefon qui son");
     }
 
     public void ConversationBegin()
     {
         isRinging = false;
+        print("ALLO");
         // Doit appeler la fonction de discussion entre police et preneur d'otages
+    }
+
+    public void endCall()
+    {
+        isRinging = false;
+        timeToAnswer = Random.Range(10, 15 + 1);
+        print("o revoar fdp");
     }
 
     public override void Use()

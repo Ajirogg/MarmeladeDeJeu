72dc762daf35e69cced44d365b5364a3757be00a
Guilhem PECH <guilhempech@gmail.com>
2019-03-01 19:46:56 -0500

    Premier jet du mouvement

---
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+    Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    public float speed = 5f;
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+}

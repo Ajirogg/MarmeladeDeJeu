31078aef635bdf2ac66865b9331d4c2cd92c030b
Guilhem PECH <guilhempech@gmail.com>
2019-03-02 22:34:49 -0500

    Solution Finale
    Extermination systematique des otages

--- ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/TriggerUse.cs
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/Scripts/TriggerUse.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class TriggerUse : MonoBehaviour
 {
+    private GameObject curFirst;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class TriggerUse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //print(FirstObject());
     }
 
     private HashSet<GameObject> objetsUtilisables = new HashSet<GameObject>();
@@ -23,81 +24,113 @@ public class TriggerUse : MonoBehaviour
 
     public  GameObject FirstObject()
     {
+        unGlowAll(ObjetsUtilisables);
+        if (ObjetsUtilisables.Count == 0)
+            return null;
+
         GameObject toReturn = ObjetsUtilisables.ToList()[0];
-        foreach (GameObject ob in objetsUtilisables)
+        foreach (GameObject ob in ObjetsUtilisables)
         {            
             if (ob.GetComponent<GroupeOtage>() != null)
                 toReturn = ob;
             else if (ob.GetComponent<Otage>() != null)
+            {
+                Glow(ob);
                 return ob;
+            }
+                
         }
+        Glow(toReturn);
         return toReturn;
     } 
 
-    private void OnTriggerEnter2D(Collider2D collider)
+
+
+    void Glow(GameObject u)
     {
-
-
-        if (collider.gameObject.GetComponent<Utilisable>() != null)
+        if (u.GetComponent<GroupeOtage>() != null)
         {
+            Glow(u.GetComponent<GroupeOtage>());
+            return;
+        }
+            
 
-            ObjetsUtilisables.Add(collider.gameObject);
+        if (u.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
+        {
+            u.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = true;
+        }
+        else
+        {
+            SpriteGlow.SpriteGlowEffect n = u.gameObject.AddComponent<SpriteGlow.SpriteGlowEffect>();
+            n.GlowBrightness = 1.38f;
+        }
+    }
 
-            if (collider.gameObject != FirstObject()) {
-                if (collider.gameObject.GetComponent<GroupeOtage>() == null)
-                    return;
+    void Glow(GroupeOtage u)
+    {
+        foreach(Otage o in u.otages)
+        {
+            Glow(o.gameObject);
+        }
+    }
 
-                foreach(Otage o in collider.gameObject.GetComponent<GroupeOtage>().otages) //Glow all hostages
-                {
-                    if (o.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
-                    {
-                        o.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = true;
-                    }
-                    else
-                    {
-                        SpriteGlow.SpriteGlowEffect n = o.gameObject.AddComponent<SpriteGlow.SpriteGlowEffect>();
-                        n.GlowBrightness = 1.38f;
-                    }
-                }
-                    
-            }
-            // Maybe improve that ?
-                
 
-            if (collider.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
+    void unGlowAll(HashSet<GameObject> set)
+    {
+        var b = GameObject.FindObjectsOfType<MonoBehaviour>();   
+
+        foreach(MonoBehaviour ob in b)
+        {
+            if (ob.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
             {
-                collider.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = true;
-            }
-            else
-            {
-                SpriteGlow.SpriteGlowEffect n= collider.gameObject.AddComponent<SpriteGlow.SpriteGlowEffect>();
-                n.GlowBrightness = 1.38f;
+                ob.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = false;
             }
         }
-    
+    }
+
+    void unGlow(GameObject go)
+    {
+        if (go.GetComponent<GroupeOtage>() != null)
+        {           
+            unGlowAll(ObjetsUtilisables);
+            return;
+        }
+
+        if (go.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
+        {
+            go.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = false;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        if (collider.gameObject.GetComponent<Utilisable>() != null)
+        {
+            ObjetsUtilisables.Add(collider.gameObject);
+            curFirst = FirstObject();
+        }        
+    }
+
+
+    void PrintObjects()
+    {
+        foreach(GameObject o in ObjetsUtilisables)
+        {
+            print(o);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.GetComponent<Utilisable>() != null)
-        {
+        
+        if (collider.gameObject.GetComponent<Utilisable>() != null){
             ObjetsUtilisables.Remove(collider.gameObject);
-
-            if(collider.gameObject.GetComponent<GroupeOtage>()!= null)
-            {
-                foreach (Otage o in collider.gameObject.GetComponent<GroupeOtage>().otages) //Glow all hostages
-                {
-                    if (o.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
-                    {
-                        o.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = false;
-                    }
-                }
-            }
-
-            if (collider.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() != null)
-            {
-                collider.gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>().enabled = false;
-            }
+            curFirst = FirstObject();
         }
+
+      
+
     }  
 }

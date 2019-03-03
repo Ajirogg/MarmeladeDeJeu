ed3dc14ddd404418697d6a845b9142836da2b778
Razania <j.ferreira1215@gmail.com>
2019-03-03 06:35:26 -0500

    Intégration des menus et Ajustement de l'ui ig

---
+++ ProjetUnityWonderjam2019/Wonderjam2019-Waterlily/Assets/UI/InGame/TimerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeCount += Time.deltaTime;
+
+        gameObject.GetComponent<Text>().text = ((int) (TimeCount / 60f)).ToString("D2") + " : " + ((int) (TimeCount % 60f)).ToString("D2");
+    }
+}
